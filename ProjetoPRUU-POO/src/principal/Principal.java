5456e969bf25cf6eeb230593bac09c521997c1f1
keiiisyanny <keisy.anny.amorim@gmail.com>
2022-07-31 08:10:44 -0300

    Alteração nomes dos usuarios

--- ProjetoPRUU-POO/src/principal/Principal.java
+++ ProjetoPRUU-POO/src/principal/Principal.java
@@ -11,47 +11,47 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
-		Administrador Adm = new Administrador ("Administrador", "adm_@gmail.com", "123546884-51");
-		Usuario Keisy = new Usuario("Keisy Anny", "keisy.amorim@gmail.com", "084545209-61");
-		Usuario Lucia = new Usuario ("Lucia Andrea", "luciasfig2@gmail.com", "094891548-06");
+		Administrador adm = new Administrador ("Administrador", "adm_@gmail.com", "123546884-51");
+		Usuario keisy = new Usuario("Keisy Anny", "keisy.amorim@gmail.com", "084545209-61");
+		Usuario lucia = new Usuario ("Lucia Andrea", "luciasfig2@gmail.com", "094891548-06");
 		
 		ArrayList <Pruu> pruuAdministrador =  new ArrayList<Pruu>();
-		Adm.setPruus(pruuAdministrador);
+		adm.setPruus(pruuAdministrador);
 		Pruu pruuAdministrador1 = new Pruu("Hoje o dia parece ótimo para um passeio a praia com os amigos!");
 		Pruu pruuAdministrador2 = new Pruu("Bora Trilhar hoje!!");
 		pruuAdministrador.add(pruuAdministrador1);
 		pruuAdministrador.add(pruuAdministrador2);
 		
 		ArrayList <Pruu> pruuKeisy =  new ArrayList<Pruu>();
-		Keisy.setPruus(pruuKeisy);
+		keisy.setPruus(pruuKeisy);
 		Pruu pruuKeisy1 = new Pruu("Vamos Festar");
 		Pruu pruuKeisy2 = new Pruu("Bora Trilhar hoje!!");
 		pruuKeisy.add(pruuKeisy1);
 		pruuKeisy.add(pruuKeisy2);
 		
 		ArrayList <Pruu> pruuLucia =  new ArrayList<Pruu>();
-		Lucia.setPruus(pruuLucia);
+		lucia.setPruus(pruuLucia);
 		Pruu pruuLucia1 = new Pruu("Vamos Festar");
 		Pruu pruuLucia2 = new Pruu("Bora Trilhar hoje!!");
 		pruuLucia.add(pruuLucia1);
 		pruuLucia.add(pruuLucia2);
 		
 		ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
-		listaUsuarios.add(Adm);
-		listaUsuarios.add(Keisy);
-		listaUsuarios.add(Lucia);
+		listaUsuarios.add(adm);
+		listaUsuarios.add(keisy);
+		listaUsuarios.add(lucia);
 		
 		listarPruus(listaUsuarios);
 		System.out.println("******* PRUUS DA LUCIA *******");
-		int quantidadePruusLucia = Lucia.getPruus().size();
-		System.out.print("Nome: " + Lucia.getNome());
+		int quantidadePruusLucia = lucia.getPruus().size();
+		System.out.print("Nome: " + lucia.getNome());
 		for (int i = 0; i < quantidadePruusLucia; i++) {
-			System.out.println(Lucia.getPruus().get(i).toString());
+			System.out.println(lucia.getPruus().get(i).toString());
 		}
 		
-		Keisy.darLike(pruuLucia1);
-		Lucia.darLike(pruuAdministrador2);
-		Adm.bloquearPruu(pruuKeisy2);
+		keisy.darLike(pruuLucia1);
+		lucia.darLike(pruuAdministrador2);
+		adm.bloquearPruu(pruuKeisy2);
 		listarPruus(listaUsuarios);
 	}
 
